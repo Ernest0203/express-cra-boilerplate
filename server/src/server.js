2b3d448ff4e28927dedad80bb96f72b24cc12eeb
Ernest0203 <ernest2332@gmail.com>
2020-06-18 14:32:53 +0300

    test

--- server/src/server.js
+++ server/src/server.js
@@ -19,10 +19,10 @@ app.use(express.json());
 //app.use('/route', route);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../../client/build'));
+  app.use(express.static('/client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve()(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../', '../', 'client', 'build', 'index.html'));
   });
 }
 
